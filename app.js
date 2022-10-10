0276bc992d3601d5495dd281530a84f20fa2784f
stevenwr92 <stevenwr92@gmail.com>
2022-10-10 20:20:12 +0700

    test

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+  require("dotenv").config();
 }
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const errHandler = require("./middleware/errHandler");
 const router = require("./routes");
 const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use("/", router)
+app.use("/", router);
 
-app.use(errHandler)
+app.use(errHandler);
 
+app.listen(port, () => {
+  console.log("start di port", port);
+});
 
-module.exports = app
+module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,21 @@
-const UserController = require('../controllers/UserController')
-const user = require('express').Router()
+const UserController = require("../controllers/UserController");
+const user = require("express").Router();
+const auth = require("../middleware/authz");
 
-user.post("/register", UserController.register)
-user.post("/login", UserController.login)
+user.post("/register", UserController.register);
+user.post("/login", UserController.login);
 
-// user.post("/balances", UserController.postBalances)
-user.get("/balances/:userId", UserController.getBalance)
-user.patch("/balances/:userId", UserController.updateBalance)
+user.use(auth);
 
-user.post("/subscriptions", UserController.postSubscription)
-user.get("/subscriptions/:id", UserController.getSubscription)
-user.patch("/subscriptions/:id", UserController.updateSubscription)
+user.post("/balances", UserController.postBalances);
+user.get("/balances/:userId", UserController.getBalance);
+user.post("/balancess", UserController.updateBalance);
 
-user.get("/:id", UserController.getUserDetail)
-user.patch("/:id", UserController.updateUser)
+user.post("/subscriptions", UserController.postSubscription);
+user.get("/subscriptions/:id", UserController.getSubscription);
+user.patch("/subscriptions/:id", UserController.updateSubscription);
 
-module.exports = user
+user.get("/:id", UserController.getUserDetail);
+user.patch("/:id", UserController.updateUser);
+
+module.exports = user;
